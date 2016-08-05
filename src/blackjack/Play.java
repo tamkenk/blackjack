0b223a35e46afee42b645b78a3a5ba2884a6494a
Ken Tam <kentam@Kens-iMac.local>
2016-08-04 22:22:25 -0700

    Return cards to deck after each round Tie doesn't count as round played

--- src/blackjack/Play.java
+++ src/blackjack/Play.java
@@ -103,9 +103,9 @@ public class Play {
 				System.out.println("Thank you for playing");
 				System.out.printf("Number of games played: %d, win: %d, lose: %d, tie: %d, ", 
 					gameCount, winCount, (gameCount-winCount-tieCount), tieCount);
-				// tie game is considered a half win 
-				System.out.printf("with winning percentage: %.2f%%",  
-					((gameCount > 0) ? ((winCount+((float)tieCount/2.0))/gameCount) * 100 : 0f));
+				// tie game is not counted as game played
+				System.out.printf("with winning percentage: %.2f%%\n",  
+					(((gameCount-tieCount) > 0) ? ((float)winCount/(gameCount-tieCount)) * 100f : 0f));
 				done = true;
 				break;
 			}
@@ -121,7 +121,7 @@ public class Play {
 		
 		// need to shuffle deck every <n> game
 		if ( 0 == (gameCount % SHUFFLE_INTERVAL) ) {
-			deck = new Deck();
+			System.out.println("shuffling cards");
 			deck.shuffle();
 		}
 
@@ -137,116 +137,37 @@ public class Play {
 		System.out.printf("Dealer's first card: %s\n", dealer.getCard(0));
 		
 		// user's play
-		boolean ret = user.draw();
-		if ( ! ret ) {
+		boolean result = user.draw();
+		Result returnVal = Result.LOSE;
+		
+		if ( ! result ) {
 			System.out.println("Your hand exceeded 21");
-			return Result.LOSE;
-		}
-		if ( user.isBlackjack() ) {
-			System.out.println("You have a blackjack hand!");
-		}
-		
-		// dealer's play
-		System.out.printf("Dealer's hand: %s, total value %d\n", dealer.displayHand(), dealer.getBestValue());
-		ret = dealer.draw();
-		System.out.printf("Dealer's final hand: %s, total value %d\n", dealer.displayHand(), dealer.getBestValue());
-		// compare hands
-		if ( ! ret ) {
-			System.out.println("Dealer's hand exceeded 21");
-			return Result.WIN;
-		}
-		else if ( dealer.getBestValue() > user.getBestValue() ) {
-			return Result.LOSE;
-		}
-		else if ( dealer.getBestValue() < user.getBestValue() ) {
-			return Result.WIN;
+			returnVal = Result.LOSE;
 		}
 		else {
-			return Result.TIE;
-		}
-	}
-	
-	@SuppressWarnings("unused")
-	private Result oneGameOLD() {
-		System.out.printf("Game: %d\n", gameCount+1);
-		
-		// need to shuffle deck every <n> game
-		if ( 0 == ((gameCount+1) % SHUFFLE_INTERVAL) ) {
-			deck.shuffle();
-		}
-		
-		Hand dealerHand = new Hand();
-		Hand userHand = new Hand();
-		
-		// deal 2 cards to each player in 2 rounds
-		userHand.addToHand(deck.getFromTop());
-		dealerHand.addToHand(deck.getFromTop());
-		userHand.addToHand(deck.getFromTop());		
-		dealerHand.addToHand(deck.getFromTop());
-		
-		System.out.printf("Dealer's first card: %s\n", dealerHand.getCard(0));
-		
-		// check for blackjack
-		if ( dealerHand.isBlackjack() ) {
-			if ( userHand.isBlackjack() ) {
-				System.out.println("Both hands have blackjack");
-				return Result.TIE;
+			// dealer's play
+			System.out.printf("Dealer's hand: %s, total value %d\n", dealer.displayHand(), dealer.getBestValue());
+			result = dealer.draw();
+			System.out.printf("Dealer's final hand: %s, total value %d\n", dealer.displayHand(), dealer.getBestValue());
+			// compare hands
+			if ( ! result ) {
+				System.out.println("Dealer's hand exceeded 21");
+				returnVal = Result.WIN;
 			}
-			System.out.printf("Dealer's hand: %s\n", dealerHand );
-			System.out.println("Dealer has blackjack");
-			return Result.LOSE;
-		}
-		if ( userHand.isBlackjack() ) {
-			if ( dealerHand.isBlackjack() ) {
-				System.out.println("Both hands have backjack");
-				return Result.TIE;
-			}						
-			System.out.printf("Your hand: %s\n", userHand );
-			System.out.println("You have blackjack");
-			return Result.WIN;
-		}
-		
-		boolean done = false;
-		while (! done) {
-			if ( userHand.isBust() ) {
-				System.out.printf("Sorry, your hand: %s, total value: %d exceeds 21\n", userHand, userHand.getBestValue());	
-				return Result.LOSE;
+			else if ( dealer.getBestValue() > user.getBestValue() ) {
+				returnVal = Result.LOSE;
 			}
-			
-			System.out.printf("Your hand: %s, total value %d\n", userHand, userHand.getBestValue());
-			
-			String action = Util.getUserInput("Hit? (y<cr>) or n<cr>) ", userInput);			
-			switch(action) {
-			case "y":
-				userHand.addToHand(deck.getFromTop());
-				break;
-			case "n":
-				done = true;
-				break;
-			}		
-		}
+			else if ( dealer.getBestValue() < user.getBestValue() ) {
+				returnVal = Result.WIN;
+			}
+			else {
+				returnVal = Result.TIE;
+			}
+		}		
+		// return cards to deck
+		user.returnCardsToDeck();
+		dealer.returnCardsToDeck();
 		
-		System.out.println("You are done");
-		System.out.printf("Dealer's hand: %s, total value %d\n", dealerHand, dealerHand.getBestValue());
-		// dealer must draw card until total is 17+ or at soft 17
-		while ( dealerHand.getBestValue() <= 16 || 
-				(dealerHand.getAceCount() > 0 && 17 == dealerHand.getBestValue()) ) {
-			dealerHand.addToHand(deck.getFromTop());
-		}
-		System.out.printf("Dealer's final hand: %s, total value %d\n", dealerHand, dealerHand.getBestValue());
-		// compare hands
-		if ( dealerHand.isBust() ) {
-			System.out.println("Dealer's hand exceeds 21");
-			return Result.WIN;
-		}
-		else if ( dealerHand.getBestValue() > userHand.getBestValue() ) {
-			return Result.LOSE;
-		}
-		else if ( dealerHand.getBestValue() < userHand.getBestValue() ) {
-			return Result.WIN;
-		}
-		else {
-			return Result.TIE;
-		}
+		return returnVal;
 	}
 }
